590ef414c027d2833c1313dd7ff84cd0a066f292
jyt312001-glitch <jyt312001@gmail.com>
2025-09-10 17:06:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+#Arithmetic operation on two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+result = num1 + num2
+result1 = num1 - num2
+result2 = num1 * num2
+result3 = num1 / num2
+print(f"The sum is: {result}")
+print(f"the subtraction is: {result1}")
+print(f"The multiplication is:{result2}")
+print(f"the division is:{result3}")
+
---
+++ app2.py
@@ -0,0 +1,5 @@
+#PERSONALISED GREETING
+name1 = (input('enter your first name: '))
+name2 =(input('enter your last name: '))
+Fullname= name1 + name2
+print("Hello, " + Fullname  + " Welcome to the python program.")
